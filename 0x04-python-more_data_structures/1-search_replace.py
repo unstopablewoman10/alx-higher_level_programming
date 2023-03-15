18940facf18ae7b305a08dc688e175cf36dd9ad5
unstopablewoman10 <hewitek@gmail.com>
2023-03-15 08:02:23 -0700

    fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if value == search else value for value in my_list]
-
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
+    return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
 
