061bf04047b269e9e833ff2be5e5002a2c913669
unstopablewoman10 <hewitek@gmail.com>
2023-03-27 11:09:45 -0700

    fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,9 +3,8 @@ import sys
 
 
 def safe_function(fct, *args):
-
     try:
         return fct(*args)
-    except Exception as exception:
-        print('Exception: {}'.format(exception), file=sys.stderr)
-
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-
+    """Returns the division of a by b."""
     try:
-        response = a / b
-    except ZeroDivisionError:
-        response = None
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
     finally:
-        print('Inside result: {}'.format(response))
-
-    return response
-
+        print("Inside result: {}".format(div))
+    return (div)
