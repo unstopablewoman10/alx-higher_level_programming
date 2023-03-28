87063c317f58a6406910958fc872d9067a00854c
unstopablewoman10 <hewitek@gmail.com>
2023-03-28 11:38:02 -0700

    fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Square Class
-This class don't do anything for the moment
-"""
+# 0-square.py by Ehoneah Obed
+"""Defines a square """
 
 
 class Square:
+    """Does nothing"""
     pass
